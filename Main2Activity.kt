14f72339dbf741baaae03d2044b3e855780b4d43
cletus4love <85235244+cletus4love@users.noreply.github.com>
2021-06-29 20:47:30 +0100

    Add files via upload

---
+++ Main2Activity.kt
@@ -0,0 +1,21 @@
+Main2Activity : 
+  import android.content.Intent 
+ import androidx.appcompat.app.AppCompatActivity
+ import android.os.Bundle
+ import kotlinx.android.synthetic.main.activity_main.*
+
+ class MainActivity : AppCompatActivity()
+ {
+ 	override fun onCreate(savedInstanceState: Bundle?)
+ 	{
+ 		super.onCreate(savedInstanceState)
+ 		setContentView(R.layout.activity2_main)
+
+ 		btn1.setOnClickListener
+ 		{ 
+ 			intent = Intent(applicationContext, MainActivity::class.java) 
+ 			startActivity(intent)
+ 		}
+
+ 	}
+ }
